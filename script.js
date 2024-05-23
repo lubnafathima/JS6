57c54b10494d4845eedec362c0e83242eb3c1462
Lubna Fathima N <lubnafathima275@gmail.com>
2024-05-23 21:49:45 +0530

    init: complete code

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let index1 = 1; index1 <= 10; index1++) {
+  let value = "";
+  for (let index2 = 1; index2 <= 7; index2++) {
+    value += "*";
+  }
+  console.log("\n");
+  console.log(value);
+}
